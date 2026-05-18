9a5342fb52cfb8d1b6d21f1c447b71e2a066667a
Vivek Hari Sridhar <vivekhsridhar@yahoo.com>
2026-05-18 14:37:32 -0700

    abm figure run

--- bistability_abm.R
+++ bistability_abm.R
@@ -18,15 +18,15 @@ set.seed(seed)
 
 # Parameters
 N <- 100            # Number of male blackbucks
-r <- 100             # Copying rate
-s <- 0.05            # Spontaneous switching rate
-Tint <- 1         # Recording interval
+r <- 20             # Copying rate
+s <- 0.02             # Spontaneous switching rate
+Tint <- 0.5         # Recording interval
 Tend <- 100         # Total simulation time
 iter <- ceiling(Tend / Tint)   # Number of recording intervals
 
 # Initial state
 # All males start at lek site 1
-x <- rep(1, N)
+x <- rep(0, N)
 
 # State trackers
 xa <- rep(0, iter)    # Number at site 1
@@ -89,7 +89,7 @@ df <- data.frame(time = time, site1 = xa, site2 = xb,
                  proportion_site1 = xa / N, proportion_site2 = xb / N, magnetization = m)
 
 # Save to CSV
-write.csv(df, file = paste0("blackbuck_gillespie_seed_", seed, ".csv"), row.names = FALSE)
+write.csv(df, file = paste0("processed_data/abm_seed_", seed, ".csv"), row.names = FALSE)
 
 # Plot
 plot(time, m, type = "l", col = "cyan4", lwd = 2, xlab = "Time", ylab = "Magnetization", main = "Blackbuck Lekking Site Consensus Dynamics")
